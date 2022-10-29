fa2bb8ad42222d0660e8b3c913e4b402bd4c0e30
Faihaan Arif <farif1234@gmail.com>
2022-10-28 20:06:35 -0400

    added spotify support

--- main.py
+++ main.py
@@ -3,13 +3,23 @@ from flask import Flask
 app = Flask(__name__)
 
 
-def urlMaker(query: str):
+def youtubeQuery(query: str):
     # print(query)
     # characters not encoded = [@,#,$,%,&,=,+,;,:,,,?,/]
     url = f"https://www.youtube.com/results?search_query={query.replace(' ', '+')}"
     return url
 
 
+def spotifyQuery(query: str):
+    url = f"https://open.spotify.com/search/{query.replace(' ', '%20')}"
+    return url
+
+
 @app.get('/youtube/<query>')
-def getURL(query):
-    return urlMaker(query)
+def getYtURL(query):
+    return youtubeQuery(query)
+
+
+@app.get('/spotify/<query>')
+def getSpURL(query):
+    return spotifyQuery(query)
